1c880f564a6bb444ae7f8ac678dd1e2ca19da593
katya <katya31195@gmail.com>
2024-08-12 23:55:37 +0300

    add header and footer

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,26 @@ document.querySelector(".contact__form__input").addEventListener("input", functi
     emailValue = document.querySelector(".contact__form__input").value;
 });
 
-function sendEmail() {
+/*function sendEmail() {
   let params = {
     email: emailValue,
   };
 
   emailjs.send("service_bs364yq", "template_j7hudjg", params);
-}
+}*/
 
 //document.querySelector(".contact__form").addEventListener("submit", sendEmail());
+
+let burgerMenu = document.querySelector(".header__menu-list");
+let burgerOpen = document.querySelector(".header__burger");
+let burgerClose = document.querySelector(".header__close-burger");
+
+burgerOpen.addEventListener("click", function () {
+  burgerMenu.style.display = "flex";
+  burgerOpen.style.display = "none";
+
+});
+burgerClose.addEventListener("click", function () {
+  burgerMenu.style.display = "none";
+  burgerOpen.style.display = "block";
+});
