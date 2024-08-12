4195e146260ba62a83e6d160d2ff5ec4c9fb6578
katya <katya31195@gmail.com>
2024-08-12 23:07:38 +0300

    add contact

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-let x;
+let emailValue;
+
+document.querySelector(".contact__form__input").addEventListener("input", function () {
+    emailValue = document.querySelector(".contact__form__input").value;
+});
+
+function sendEmail() {
+  let params = {
+    email: emailValue,
+  };
+
+  emailjs.send("service_bs364yq", "template_j7hudjg", params);
+}
+
+//document.querySelector(".contact__form").addEventListener("submit", sendEmail());
