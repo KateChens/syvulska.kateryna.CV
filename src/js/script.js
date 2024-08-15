91f2b7e5b9203508a32500b660bca08f267b0ad0
katya <katya31195@gmail.com>
2024-08-16 00:20:19 +0300

    add visual improvements

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,17 @@ document.querySelector(".contact__form__input").addEventListener("input", functi
 
 //document.querySelector(".contact__form").addEventListener("submit", sendEmail());
 
+const modalsBlur = document.getElementsByClassName("modal-backdrop")[0];
+const startToVolunteerBody = document.getElementsByTagName("body")[0];
+function settingsForModalsOpen() {
+  modalsBlur.style.display = "block";
+  startToVolunteerBody.style.overflow = "hidden";
+}
+function settingsForModalsClose() {
+  startToVolunteerBody.style.overflow = "auto";
+  modalsBlur.style.display = "none";
+}
+
 let burgerMenu = document.querySelector(".header__menu-list");
 let burgerOpen = document.querySelector(".header__burger");
 let burgerClose = document.querySelector(".header__close-burger");
@@ -21,9 +32,24 @@ let burgerClose = document.querySelector(".header__close-burger");
 burgerOpen.addEventListener("click", function () {
   burgerMenu.style.display = "flex";
   burgerOpen.style.display = "none";
-
+  settingsForModalsOpen();
 });
 burgerClose.addEventListener("click", function () {
   burgerMenu.style.display = "none";
   burgerOpen.style.display = "block";
+  settingsForModalsClose();
 });
+
+// top-button start
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("on-top").style.display = "block";
+    } else {
+        document.getElementById("on-top").style.display = "none";
+    }
+}
+// top-button end
